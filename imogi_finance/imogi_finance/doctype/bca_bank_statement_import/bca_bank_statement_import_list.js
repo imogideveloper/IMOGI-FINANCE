82d533d30efaa997ef189aa029f0cdd7f4fb89d9
AbisenaP <91299253+Abisena@users.noreply.github.com>
2025-12-23 13:16:44 +0700

    Align bank statement list columns with Odoo layout

--- imogi_finance/imogi_finance/doctype/bca_bank_statement_import/bca_bank_statement_import_list.js
+++ imogi_finance/imogi_finance/doctype/bca_bank_statement_import/bca_bank_statement_import_list.js
@@ -1,32 +1,112 @@
-const ensureImportedOnColumn = (listview) => {
-  if (!listview || !Array.isArray(listview.columns) || !listview.columns.length) return;
+const formatDate = (value) => (value ? frappe.datetime.str_to_user(value).split(' ')[0] : '');
 
+const formatCurrency = (value) => frappe.format(value, { fieldtype: 'Currency' }, { inline: true });
+
+const getColumn = (listview, fieldname, fallback) => {
   const existingIndex = listview.columns.findIndex(
-    ({ fieldname, id }) => fieldname === 'imported_on' || id === 'imported_on',
+    ({ fieldname: current, id }) => current === fieldname || id === fieldname,
   );
 
-  const importedOnColumn =
-    existingIndex > -1
-      ? listview.columns.splice(existingIndex, 1)[0]
-      : {
-          id: 'imported_on',
-          fieldname: 'imported_on',
-          label: __('Imported On'),
-          fieldtype: 'Datetime',
-          width: 180,
-        };
+  if (existingIndex > -1) {
+    return listview.columns.splice(existingIndex, 1)[0];
+  }
 
-  listview.columns.splice(1, 0, importedOnColumn);
-  listview.datatable?.setColumns(listview.columns);
-  listview.datatable?.refresh(listview.data);
+  return fallback;
+};
+
+const refreshColumns = (listview) => {
+  if (!listview || !Array.isArray(listview.columns) || !listview.columns.length || !listview.datatable)
+    return;
+
+  const idColumn = getColumn(listview, 'name', {
+    id: 'name',
+    fieldname: 'name',
+    label: __('Reference'),
+    width: 230,
+  });
+
+  const importedOnColumn = getColumn(listview, 'imported_on', {
+    id: 'imported_on',
+    fieldname: 'imported_on',
+    label: __('Date'),
+    fieldtype: 'Datetime',
+    width: 120,
+    format: (value) => formatDate(value),
+  });
+
+  const bankAccountColumn = getColumn(listview, 'bank_account', {
+    id: 'bank_account',
+    fieldname: 'bank_account',
+    label: __('Journal'),
+    width: 180,
+    link_field: 'bank_account',
+    fieldtype: 'Link',
+  });
+
+  const companyColumn = getColumn(listview, 'company', {
+    id: 'company',
+    fieldname: 'company',
+    label: __('Company'),
+    width: 180,
+    link_field: 'company',
+    fieldtype: 'Link',
+  });
+
+  const startingBalanceColumn = getColumn(listview, 'starting_balance', {
+    id: 'starting_balance',
+    fieldname: 'starting_balance',
+    label: __('Starting Balance'),
+    fieldtype: 'Currency',
+    width: 170,
+    align: 'right',
+    format: (value) => formatCurrency(value),
+  });
+
+  const endingBalanceColumn = getColumn(listview, 'ending_balance', {
+    id: 'ending_balance',
+    fieldname: 'ending_balance',
+    label: __('Ending Balance'),
+    fieldtype: 'Currency',
+    width: 170,
+    align: 'right',
+    format: (value) => formatCurrency(value),
+  });
+
+  const statusColumn = getColumn(listview, 'import_status', {
+    id: 'import_status',
+    fieldname: 'import_status',
+    label: __('Status'),
+    width: 140,
+  });
+
+  listview.columns = [
+    idColumn,
+    importedOnColumn,
+    bankAccountColumn,
+    companyColumn,
+    startingBalanceColumn,
+    endingBalanceColumn,
+    statusColumn,
+  ];
+
+  listview.datatable.setColumns(listview.columns);
+  listview.datatable.refresh(listview.get_data());
 };
 
 frappe.listview_settings['BCA Bank Statement Import'] = {
-  add_fields: ['imported_on', 'company', 'bank_account', 'import_status', 'hash_id'],
+  add_fields: [
+    'imported_on',
+    'company',
+    'bank_account',
+    'import_status',
+    'hash_id',
+    'starting_balance',
+    'ending_balance',
+  ],
   onload(listview) {
-    ensureImportedOnColumn(listview);
+    refreshColumns(listview);
   },
   refresh(listview) {
-    ensureImportedOnColumn(listview);
+    refreshColumns(listview);
   },
 };
