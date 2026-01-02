ab71fd30566573334ee40da4fd86ef9d2bb4c265
dannyaudian <danny.a.pratama@gmail.com>
2026-01-02 14:26:34 +0700

    Default Vision endpoint and drop project/location settings

--- imogi_finance/imogi_finance/doctype/tax_invoice_ocr_settings/tax_invoice_ocr_settings.py
+++ imogi_finance/imogi_finance/doctype/tax_invoice_ocr_settings/tax_invoice_ocr_settings.py
@@ -11,10 +11,8 @@ from frappe.model.document import Document
 class TaxInvoiceOCRSettings(Document):
     def validate(self):
         if self.ocr_provider == "Google Vision":
-            if not self.google_vision_api_key:
-                frappe.throw(_("Google Vision API Key is required when provider is Google Vision."))
-            if not self.google_vision_endpoint:
-                frappe.throw(_("Google Vision endpoint is required when provider is Google Vision."))
+            if not self.google_vision_service_account_file:
+                frappe.throw(_("Google Vision Service Account File is required when provider is Google Vision."))
 
         if self.ocr_provider == "Tesseract" and not self.tesseract_cmd:
             frappe.throw(_("Tesseract command/path is required when provider is Tesseract."))
