ee591b4a58564a8550fd1a5ad614672f4d871b87
AbisenaP <91299253+Abisena@users.noreply.github.com>
2026-01-13 16:28:15 +0700

    Update expense_request.js

--- imogi_finance/imogi_finance/doctype/expense_request/expense_request.js
+++ imogi_finance/imogi_finance/doctype/expense_request/expense_request.js
@@ -145,7 +145,7 @@ function computeTotals(frm) {
     || (frm.doc.is_pph_applicable ? flt(frm.doc.pph_base_amount || 0) : 0);
   const pphRate = flt(frm._pph_rate || 0);
   const totalPph = pphRate ? (pphBaseTotal * pphRate) / 100 : pphBaseTotal;
-  const totalAmount = totalExpense + totalAsset + totalPpn + totalPpnbm + totalPph;
+  const totalAmount = totalExpense + totalAsset + totalPpn + totalPpnbm - totalPph;
 
   return {
     totalExpense,
